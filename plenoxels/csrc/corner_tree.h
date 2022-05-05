8c1e6487bc4b2e58929a651be708f83fdbe328d6
gmeanti <giacomo.meanti@gmail.com>
2022-05-04 17:17:18 -0700

    Fix

--- plenoxels/csrc/corner_tree.h
+++ plenoxels/csrc/corner_tree.h
@@ -562,7 +562,7 @@ RenderingOutput gen_samples(
     const uint32_t batch_size = rays_o.size(0);
 
     auto ray_steps = torch::full({batch_size, opt.max_intersections}, -1.0,
-            torch::dtype(torch::kFloat32).device(data.device()));
+            torch::dtype(torch::kFloat32).device(t_child.device()));
     auto num_intersections = torch::zeros({batch_size}, torch::dtype(torch::kInt32).device(data.device()));
     auto positions = torch::empty({batch_size, opt.max_intersections, 3},
             torch::dtype(torch::kFloat32).device(data.device()));
