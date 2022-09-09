210a72f075e14ba1918d09cb2f87aba79a675afe
gmeanti <giacomo.meanti@gmail.com>
2022-09-09 09:26:03 +0200

    Merge sara changes to video

--- plenoxels/datasets/llff_video_dataset.py
+++ plenoxels/datasets/llff_video_dataset.py
@@ -25,7 +25,7 @@ class VideoDataset(BaseDataset):
 
     def __init__(self, datadir, split='train', downsample=1.0, subsample_time=1):
         super().__init__(datadir=datadir,
-                         scene_bbox=torch.tensor([[-5.0, -3.0, -3.0], [3, 3, 100.0]]),
+                         scene_bbox=torch.tensor([[-30.0, -3.0, -2.0], [3, 80, 100.0]]),
                          split=split,
                          is_ndc=True)
         # subsample_time (in [0,1]) lets you use a random percentage of the frames from each video
@@ -49,13 +49,7 @@ class VideoDataset(BaseDataset):
 
     def load_image(self, img, out_h, out_w):
         img = self.tensor2pil(img)
-        if out_h is None:
-            out_h = int(img.size[0] / self.downsample)
-            self.img_h = out_h
-        if out_w is None:
-            out_w = int(img.size[1] / self.downsample)
-            self.img_w = out_w
-        img = img.resize((out_h, out_w), Image.LANCZOS)
+        img = img.resize((out_w, out_h), Image.LANCZOS)
         img = self.pil2tensor(img)  # [C, H, W]
         img = img.permute(1, 2, 0)  # [H, W, C]
         return img
@@ -63,7 +57,7 @@ class VideoDataset(BaseDataset):
     def load_from_disk(self):
         poses_bounds = np.load(os.path.join(self.datadir, 'poses_bounds.npy'))  # [n_cameras, 17]
         imgs, poses, timestamps = [], [], []
-        videopaths = glob.glob(os.path.join(self.datadir, '*.mp4'))  # [n_cameras]
+        videopaths = np.array(glob.glob(os.path.join(self.datadir, '*.mp4')))  # [n_cameras]
         assert len(poses_bounds) == len(videopaths), \
             'Mismatch between number of cameras and number of poses!'
         videopaths.sort()
@@ -73,11 +67,8 @@ class VideoDataset(BaseDataset):
             poses_bounds = poses_bounds[1:]
             videopaths = videopaths[1:]
         else:
-            # poses_bounds = [poses_bounds[0]]
-            # videopaths = [videopaths[0]]
-            # Eval on all the views just so we can see more pictures
-            poses_bounds = poses_bounds[0:]
-            videopaths = videopaths[0:]
+            poses_bounds = poses_bounds[0].reshape(1, 17)
+            videopaths = [videopaths[0]]
 
         poses = poses_bounds[:, :15].reshape(-1, 3, 5)  # [N_cameras, 3, 5]
         self.near_fars = poses_bounds[:, -2:]  # [N_cameras, 2]
@@ -103,26 +94,28 @@ class VideoDataset(BaseDataset):
         self.near_fars /= scale_factor
         poses[..., 3] /= scale_factor
 
+        all_poses = []
         for camera_id in tqdm(range(len(videopaths))):
             cam_video = imageio.get_reader(videopaths[camera_id], 'ffmpeg')
             for frame_idx, frame in enumerate(cam_video):
                 if frame_idx > len_time:
-                    len_time = frame_idx
+                    len_time = frame_idx + 1
                 # Decide whether to keep this frame or not
-                # if np.random.uniform() > self.subsample_time:
-                #     continue
+                if np.random.uniform() > self.subsample_time:
+                    continue
                 # Only keep frame zero, for debugging
-                if frame_idx > 0:
-                    len_time = 300
-                    break
+                # if frame_idx > 0:
+                #     len_time = 300
+                #     break
                 # Do any downsampling on the image
-                img = self.load_image(frame, self.img_h, self.img_w)
+                img = self.load_image(frame, self.intrinsics.height, self.intrinsics.width)
                 imgs.append(img)
                 timestamps.append(frame_idx)
+                all_poses.append(torch.from_numpy(poses[camera_id]).float())
         self.len_time = len_time
         imgs = torch.stack(imgs, 0)  # [N, H, W, 3]
         timestamps = torch.tensor(timestamps)  # [N]
-        poses = [torch.from_numpy(pose).float() for pose in poses]  # [N, 3, 4]
+        poses = all_poses  # [N, 3, 4]
 
         log.info(f"LLFFDataset - Loaded {self.split} set from {self.datadir}: {len(imgs)} "
                  f"images of size {imgs[0].shape}. {intrinsics}")
--- plenoxels/runners/video_trainer.py
+++ plenoxels/runners/video_trainer.py
@@ -1,15 +1,21 @@
 import logging
 import math
+import os
 from collections import defaultdict
 from typing import Dict, Optional
 
 import numpy as np
+import pandas as pd
 import torch
 import torch.utils.data
+from tqdm import tqdm
+import cv2
 
 from plenoxels.ema import EMA
 from plenoxels.models.lowrank_video import LowrankVideo
 from .multiscene_trainer import Trainer
+from ..ops.image import metrics
+from ..ops.image.io import write_png, write_exr
 
 
 class VideoTrainer(Trainer):
@@ -25,6 +31,7 @@ class VideoTrainer(Trainer):
                  train_fp16: bool,
                  save_every: int,
                  valid_every: int,
+                 save_video: bool,
                  **kwargs
                  ):
         # Keys we wish to ignore
@@ -44,6 +51,7 @@ class VideoTrainer(Trainer):
                          valid_every=valid_every,
                          transfer_learning=False,  # No transfer with video
                          **kwargs)
+        self.save_video = save_video
 
     def eval_step(self, data, dset_id) -> torch.Tensor:
         """
@@ -115,6 +123,80 @@ class VideoTrainer(Trainer):
                      f"{sum(np.prod(p.shape) for p in model.parameters()):,} parameters.")
         model.cuda()
         return model
+
+    def validate(self):
+        val_metrics = []
+        with torch.no_grad():
+            for dset_id, dataset in enumerate(self.test_datasets):
+                per_scene_metrics = {
+                    "psnr": 0,
+                    "ssim": 0,
+                    "dset_id": dset_id,
+                }
+
+                pred_frames = []
+                pb = tqdm(total=len(dataset), desc=f"Test scene {dset_id} ({dataset.name})")
+                for img_idx, data in enumerate(dataset):
+                    preds = self.eval_step(data, dset_id=dset_id)
+                    gt = data[2]
+                    out_metrics, out_img = self.evaluate_metrics(
+                        gt, preds, dset_id=dset_id, dset=dataset, img_idx=img_idx, name=None)
+                    pred_frames.append(out_img)
+                    per_scene_metrics["psnr"] += out_metrics["psnr"]
+                    per_scene_metrics["ssim"] += out_metrics["ssim"]
+                    pb.set_postfix_str(f"PSNR={out_metrics['psnr']:.2f}", refresh=False)
+                    pb.update(1)
+                pb.close()
+                per_scene_metrics["psnr"] /= len(dataset)  # noqa
+                per_scene_metrics["ssim"] /= len(dataset)  # noqa
+                log_text = f"EPOCH {self.epoch}/{self.num_epochs} | scene {dset_id}"
+                log_text += f" | D{dset_id} PSNR: {per_scene_metrics['psnr']:.2f}"
+                log_text += f" | D{dset_id} SSIM: {per_scene_metrics['ssim']:.6f}"
+                logging.info(log_text)
+                val_metrics.append(per_scene_metrics)
+        df = pd.DataFrame.from_records(val_metrics)
+        df.to_csv(os.path.join(self.log_dir, f"test_metrics_epoch{self.epoch}.csv"))
+
+    def write_video_to_file(self, frames, dataset):
+        if self.save_video:
+            video_file = os.path.join(self.log_dir, f"epoch{self.epoch}.mp4")
+            logging.info(f"Saving video to {video_file}")
+            video = cv2.VideoWriter(
+                video_file, cv2.VideoWriter_fourcc(*'mp4v'), 30,
+                (2 * dataset.intrinsics.width, dataset.intrinsics.height))
+            for img in frames:
+                video.write(img[:, :, ::-1])  # opencv uses BGR instead of RGB
+            cv2.destroyAllWindows()
+            video.release()
+
+    def evaluate_metrics(self, gt, preds: torch.Tensor, dset, dset_id, img_idx, name=None):
+        gt = gt.reshape(dset.intrinsics.height, dset.intrinsics.width, -1).cpu()
+        if gt.shape[-1] == 4:
+            gt = gt[..., :3] * gt[..., 3:] + (1.0 - gt[..., 3:])
+        preds = preds.reshape(dset.intrinsics.height, dset.intrinsics.width, 3).cpu()
+        err = (gt - preds) ** 2
+        exrdict = {
+            "gt": gt.numpy(),
+            "preds": preds.numpy(),
+            "err": err.numpy(),
+        }
+
+        summary = {
+            "mse": torch.mean(err),
+            "psnr": metrics.psnr(preds, gt),
+            "ssim": metrics.ssim(preds, gt),
+        }
+
+        out_name = f"epoch{self.epoch}-D{dset_id}-{img_idx}"
+        if name is not None and name != "":
+            out_name += "-" + name
+
+        out_img = (preds * 255.0).byte().numpy()
+        if not self.save_video:
+            write_exr(os.path.join(self.log_dir, out_name + ".exr"), exrdict)
+            write_png(os.path.join(self.log_dir, out_name + ".png"), out_img)
+
+        return summary, out_img
 
 
 def losses_to_postfix(loss_dict: Dict[str, EMA]) -> str:
