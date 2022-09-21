f7acc15da9b5906de16f7a4590699437e6fd0dc6
Giacomo <giacomo.meanti@gmail.com>
2022-09-21 21:45:45 +0200

    Push updated config (added l1_density_weight and density multiplier)

--- plenoxels/configs/giac_learnablehash.py
+++ plenoxels/configs/giac_learnablehash.py
@@ -19,14 +19,15 @@ config = {
     "num_epochs": 10,
     "batch_size": 4096,
     "num_batches_per_dset": 1,
-    "scheduler_type": "cosine",
+    "scheduler_type": None,
     "optim_type": "adam",
-    "lr": 2e-2,
-    "regnerf_weight_start": 0,
-    "regnerf_weight_end": 0,
-    "regnerf_weight_max_step": 512,
+    "lr": 2e-3,
+    "regnerf_weight_start": 0.0,
+    "regnerf_weight_end": 0.0,
+    "regnerf_weight_max_step": 700,
 
     "plane_tv_weight": 0.0,
+    "l1density_weight": 0.0,
 
     # Training settings
     "train_fp16": True,
@@ -36,30 +37,31 @@ config = {
     "transfer_learning": False,
 
     # Raymarching settings
-    "raymarch_type": "fixed",
-    "num_sample_multiplier": 1,  # Used when raymarch_type is 'voxel_size'
+    "raymarch_type": "voxel_size",
+    "num_sample_multiplier": 2,  # Used when raymarch_type is 'voxel_size'
     "n_intersections": 440,  # Used when raymarch_type is 'fixed'
     "spacing_fn": "linear",
     "single_jitter": True,
 
     # Model settings
     "density_threshold": 1e-4,
-    "dmask_update": [800],
+    "dmask_update": [800, 6000, 12000],
+    "density_multiplier": 1,
     "grid_config": """
 [
     {
         "input_coordinate_dim": 3,
-        "output_coordinate_dim": 3,
+        "output_coordinate_dim": 5,
         "grid_dimensions": 3,
         "resolution": [282, 314, 188],
         "rank": 1,
         "init_std": 0.01,
     },
     {
-        "input_coordinate_dim": 3,
-        "resolution": [10, 10, 10],
+        "input_coordinate_dim": 5,
+        "resolution": [4, 4, 4, 4, 4],
         "feature_dim": 32,
-        "init_std": 0.05
+        "init_std": 0.05,
     }
 ]
 """
