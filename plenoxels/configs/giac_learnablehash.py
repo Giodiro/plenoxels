cc2a47d3e0933b328c9e4f8c787ff8d948537889
giacomo m <giacomo.meanti@gmail.com>
2022-10-03 08:00:30 +0200

    Commit config

--- plenoxels/configs/giac_learnablehash.py
+++ plenoxels/configs/giac_learnablehash.py
@@ -1,12 +1,12 @@
 # configuration file to be used with `main.py` for normal (or multiscene) training
 # the configuration must be specified in a dictionary called `config`.
 config = {
-    "expname": "lego_test",
+    "expname": "fern_test_large_noreg",
     "logdir": "./logs",
 
     # Data settings
     "data_resolution": None,
-    "data_downsample": 8,
+    "data_downsample": 4,
     #"data_dirs": ["/data/DATASETS/SyntheticNerf/lego"],
     "data_dirs": ["/data/DATASETS/LLFF/fern"],
     # Data settings for 360
@@ -19,15 +19,17 @@ config = {
     "num_epochs": 10,
     "batch_size": 4096,
     "num_batches_per_dset": 1,
-    "scheduler_type": None,
+    "scheduler_type": "step",
     "optim_type": "adam",
-    "lr": 2e-3,
-    "regnerf_weight_start": 0.0,
-    "regnerf_weight_end": 0.0,
-    "regnerf_weight_max_step": 700,
+    "lr": 8e-3,
+    "regnerf_weight_start": 000.0,
+    "regnerf_weight_end": 0.00,
+    "regnerf_weight_max_step": 512,
 
     "plane_tv_weight": 0.0,
     "l1density_weight": 0.0,
+    "volume_tv_weight": 0.0,
+    "volume_tv_npts": 100,
 
     # Training settings
     "train_fp16": True,
@@ -38,28 +40,32 @@ config = {
 
     # Raymarching settings
     "raymarch_type": "voxel_size",
-    "num_sample_multiplier": 2,  # Used when raymarch_type is 'voxel_size'
+    "num_sample_multiplier": 1,  # Used when raymarch_type is 'voxel_size'
     "n_intersections": 440,  # Used when raymarch_type is 'fixed'
     "spacing_fn": "linear",
     "single_jitter": True,
 
     # Model settings
     "density_threshold": 1e-4,
-    "dmask_update": [800, 6000, 12000],
+    "dmask_update": [2500],
+    "upsample_steps": [2000,3000,4000,5500], #[500, 800, 1200, 1500, 2000],
+    #"dmask_update": [2000, 4000],
+    #"upsample_steps": [2000, 3000, 4000, 5500, 7000],
+    "upsample_resolution": [6941900, 23292360, 78205680, 261365436], #[3241792, 5832000, 11239424, 16777216, 27000000], #[4410944, 11239424, 32768000, 91125000],
     "density_multiplier": 1,
     "grid_config": """
 [
     {
         "input_coordinate_dim": 3,
         "output_coordinate_dim": 5,
-        "grid_dimensions": 3,
-        "resolution": [282, 314, 188],
-        "rank": 1,
-        "init_std": 0.01,
+        "grid_dimensions": 2,
+        "resolution": [141, 157, 94],
+        "rank": 10,
+        "init_std": 0.2,
     },
     {
         "input_coordinate_dim": 5,
-        "resolution": [4, 4, 4, 4, 4],
+        "resolution": [6, 6, 6, 6, 6],
         "feature_dim": 32,
         "init_std": 0.05,
     }
