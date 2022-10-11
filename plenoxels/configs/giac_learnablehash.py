cd2f2bb68be8ec340d2f03fd03349e0a55428f30
giacomo m <giacomo.meanti@gmail.com>
2022-10-11 21:49:29 +0200

    Config

--- plenoxels/configs/giac_learnablehash.py
+++ plenoxels/configs/giac_learnablehash.py
@@ -1,16 +1,16 @@
 # configuration file to be used with `main.py` for normal (or multiscene) training
 # the configuration must be specified in a dictionary called `config`.
 config = {
-    "expname": "lego_test",
+    "expname": "materials_test",
     "logdir": "./logs",
 
     # Data settings
     "data_resolution": None,
-    "data_downsample": 3,
-    "data_dirs": ["/data/DATASETS/SyntheticNerf/lego"],
+    "data_downsample": 1,
+    "data_dirs": ["/data/DATASETS/SyntheticNerf/materials"],
     #"data_dirs": ["/data/DATASETS/LLFF/fern"],
     # Data settings for 360
-    "max_tr_frames": 20,
+    "max_tr_frames": 100,
     "max_ts_frames": 10,
     # Data settings for LLFF
     "hold_every": 8,
@@ -21,13 +21,13 @@ config = {
     "num_batches_per_dset": 1,
     "scheduler_type": "step",
     "optim_type": "adam",
-    "lr": 1e-2,
+    "lr": 2e-2,
 
     "regnerf_weight_start": 0.0,
     "regnerf_weight_end": 0.0,
     "regnerf_weight_max_step": 700,
 
-    "plane_tv_weight": 0.00,
+    "plane_tv_weight": 0.0005,
     "plane_tv_what": "Gcoords",
 
     "l1density_weight": 0.000,
@@ -53,12 +53,12 @@ config = {
 
     # Model settings
     "sh": True,
-    "density_threshold": 1e-4,
-    "dmask_update": [500],
-    "upsample_steps": [],#2000,3000,4000,5500], #[500, 800, 1200, 1500, 2000],
+    "density_threshold": 4e-4,
+    "dmask_update": [3500, 5500],
+    "upsample_steps": [7000,10000],#,4000,5500], #[500, 800, 1200, 1500, 2000],
     #"dmask_update": [2000, 4000],
     #"upsample_steps": [2000, 3000, 4000, 5500, 7000],
-    "upsample_resolution": [],#6941900, 23292360, 78205680, 261365436], #[3241792, 5832000, 11239424, 16777216, 27000000], #[4410944, 11239424, 32768000, 91125000],
+    "upsample_resolution": [6941900, 23292360],#, 78205680, 261365436], #[3241792, 5832000, 11239424, 16777216, 27000000], #[4410944, 11239424, 32768000, 91125000],
     "density_multiplier": 1,
     "grid_config": """
 [
@@ -66,7 +66,7 @@ config = {
         "input_coordinate_dim": 3,
         "output_coordinate_dim": 5,
         "grid_dimensions": 2,
-        "resolution": [141, 157, 94],
+        "resolution": [128, 128, 128],
         "rank": 10,
         "init_std": 0.2,
     },
