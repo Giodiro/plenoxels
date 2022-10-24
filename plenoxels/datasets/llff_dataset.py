1ce0a0ed0039d5f53823212518112c0e1a5c5dcf
gmeanti <giacomo.meanti@gmail.com>
2022-10-24 09:19:16 +0200

    LLFF dataset to use contraction

--- plenoxels/datasets/llff_dataset.py
+++ plenoxels/datasets/llff_dataset.py
@@ -7,24 +7,22 @@ import numpy as np
 import torch
 
 from .data_loading import parallel_load_images
-from .ray_utils import get_ray_directions_blender, get_rays, ndc_rays_blender, center_poses, generate_spiral_path
+from .ray_utils import (
+    get_ray_directions_blender, get_rays, ndc_rays_blender, center_poses,
+    gen_camera_dirs
+)
 from .intrinsics import Intrinsics
 from .base_dataset import BaseDataset
-from .patchloader import PatchLoader
 
 
 class LLFFDataset(BaseDataset):
-    patchloader: Optional[PatchLoader]
-
     def __init__(self,
                  datadir,
                  split: str,
                  dset_id: int,
                  batch_size: Optional[int] = None,
                  downsample: int = 4,
-                 hold_every: int = 8,
-                 patch_size: Optional[int] = 8,
-                 extra_views: bool = False):
+                 hold_every: int = 8):
         """
         spheric_poses: whether the images are taken in a spheric inward-facing manner
                        default: False (forward-facing)
@@ -33,49 +31,61 @@ class LLFFDataset(BaseDataset):
 
         self.downsample = downsample
         self.hold_every = hold_every
-        self.patch_size = patch_size
         self.dset_id = dset_id
         self.near_far = [0.0, 1.0]
-        self.extra_views = split == 'train' and extra_views
-        self.patchloader = None
 
         image_paths, poses, near_fars, intrinsics = load_llff_poses(
             datadir, downsample=downsample, split=split, hold_every=hold_every, near_scaling=1.0)
         imgs = load_llff_images(image_paths, intrinsics, split)
-        rays_o, rays_d, imgs = create_llff_rays(
-            imgs=imgs, poses=poses, intrinsics=intrinsics, merge_all=split == 'train')
+        if split == 'train':
+            imgs = imgs.view(-1, imgs.shape[-1])
+        else:
+            imgs = imgs.view(-1, intrinsics.height * intrinsics.width, imgs.shape[-1])
         super().__init__(datadir=datadir,
                          split=split,
-                         scene_bbox=self.init_bbox(datadir),
+                         scene_bbox=torch.tensor([[-2, -2, -2], [2, 2, 2]]),
                          is_ndc=True,
                          batch_size=batch_size,
                          imgs=imgs,
-                         rays_o=rays_o,
-                         rays_d=rays_d,
-                         intrinsics=intrinsics)
-
-        if self.extra_views:
-            extra_poses = generate_spiral_path(poses.numpy(), near_fars, n_frames=120)
-            extra_rays_o, extra_rays_d, _ = create_llff_rays(
-                imgs=None, poses=extra_poses, intrinsics=intrinsics, merge_all=False)
-            extra_rays_o = extra_rays_o.view(-1, self.img_h, self.img_w, 3)
-            extra_rays_d = extra_rays_d.view(-1, self.img_h, self.img_w, 3)
-            self.patchloader = PatchLoader(extra_rays_o, extra_rays_d, len_time=None,
-                                           batch_size=self.batch_size, patch_size=self.patch_size,
-                                           generator=self.generator)
-
+                         rays_o=None,
+                         rays_d=None,
+                         intrinsics=intrinsics,
+                         is_contracted=True)
         log.info(f"LLFFDataset - Loaded {split} set from {datadir}: {len(poses)} images of "
                  f"shape {self.img_h}x{self.img_w} with {imgs.shape[-1]} channels. {intrinsics}")
 
-    def init_bbox(self, datadir):
-        return torch.tensor([[-1.5, -1.67, -1.0], [1.5, 1.67, 1.0]])
-
     def __getitem__(self, index):
-        out = super().__getitem__(index)
-        out["dset_id"] = self.dset_id
-        if self.split == 'train' and self.extra_views:
-            out.update(self.patchloader[index])
-        return out
+        h = self.intrinsics.height
+        w = self.intrinsics.width
+        dev = "cpu"
+        if self.split == 'train':
+            index = self.get_rand_ids(index)
+            image_id = torch.div(index, h * w, rounding_mode='floor')
+            y = torch.remainder(index, h * w).div(w, rounding_mode='floor')
+            x = torch.remainder(index, h * w).remainder(w)
+        else:
+            image_id = [index]
+            x, y = torch.meshgrid(
+                torch.arange(w, device=dev),
+                torch.arange(h, device=dev),
+                indexing="xy",
+            )
+            x = x.flatten()
+            y = y.flatten()
+
+        rgba = self.imgs[index] / 255.0  # (num_rays, 3)   this converts to f32
+        c2w = self.poses[image_id]       # (num_rays, 3, 4)
+        camera_dirs = gen_camera_dirs(
+            x, y, self.intrinsics, True)  # [num_rays, 3]
+
+        directions = (camera_dirs[:, None, :] * c2w[:, :3, :3]).sum(dim=-1)
+        origins = torch.broadcast_to(c2w[:, :3, -1], directions.shape)
+        return {
+            "rays_o": origins.reshape(-1, 3),
+            "rays_d": directions.reshape(-1, 3),
+            "imgs": rgba.reshape(-1, rgba.shape[-1]),
+            "dset_id": self.dset_id,
+        }
 
 
 def _split_poses_bounds(poses_bounds: np.ndarray) -> Tuple[np.ndarray, np.ndarray, Intrinsics]:
