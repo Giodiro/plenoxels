37923002d1395dcca20c5e31350705aa2a2d260c
Frederik Rahbaek Warburg <frewar1905@gmail.com>
2022-11-02 04:24:49 +0100

    better bounds

--- plenoxels/datasets/photo_tourism.py
+++ plenoxels/datasets/photo_tourism.py
@@ -55,8 +55,8 @@ class PhotoTourismDataset(torch.utils.data.Dataset):
         self.downsample = downsample
         self.near_far = [0.0, 1.0]
         self.keyframes = False
-        self.is_ndc = True
-        self.is_contracted = False
+        self.is_ndc = False
+        self.is_contracted = True
         self.lookup_time = True
         if self.is_contracted:
             self.scene_bbox = torch.tensor([[-2.0,-2.0,-2.0], [2.0,2.0,2.0]])
@@ -67,8 +67,8 @@ class PhotoTourismDataset(torch.utils.data.Dataset):
         self.name = os.path.basename(datadir)
         self.datadir = datadir
         # TODO: tune these for each dataset
-        self.global_translation = torch.tensor([0, 0, 0])
-        self.global_scale = torch.tensor([1, 1, 1])
+        self.global_translation = torch.tensor([0, 0, 0.7])
+        self.global_scale = torch.tensor([1.5, 1.5, 1.5])
 
         # read all files in the tsv first (split to train and test later)
         tsv = glob.glob(os.path.join(self.datadir, '*.tsv'))[0]
