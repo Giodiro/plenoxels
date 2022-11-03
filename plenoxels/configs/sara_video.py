9b49e657d5509160a8ecc71fdb17d8e4101aee27
Sara Fridovich-Keil <sfk@eecs.berkeley.edu>
2022-11-02 23:04:40 -0700

    proposal sampling for video

--- plenoxels/configs/sara_video.py
+++ plenoxels/configs/sara_video.py
@@ -1,12 +1,8 @@
 # configuration file to be used with `main.py` for video training
 config = {
-    # "expname": "legovideo20views_regdepthweightedacc_400_0.1_512_3framesreso3rank20",
-    # "expname": "contracted2e2_downsample4reso300_keyframes6k_isg30k_ist40kalpha0.01_lr0.1_step",
-    # "expname": "contracted3-10_keyframes5k_planetv0.0001",
-    # "expname": "noF_downsample4reso300cubesample800contractedadaptivereciprocal_keyframes20k_isg20k_ist20k_rank10_planetv0.001",
-    # "expname": "newmodel300.300.300.150_noF_rank2_reg20k_isg20k_lr0.01_planetv0.1_sample400_init0.1to0.2",
-    "expname": "test_increase_trainable_rank",
-    # "expname": "testrelu_sameranktimereso128_llff",
+    # "expname": "newmodel500.500.500.150_noF_rank2_reg30k_isg30k_ist30k_lr0.01cosine_l1sigma0.1_planetv0.05_sample400_init0.1to0.5",
+    # "expname": "multiscale64to512.150_rank4_reg20k_isg20k_ist20k_lr0.01_timesmooth1_planetv0_scale0.6xyshift2z",
+    "expname": "testproposal",
     "logdir": "./logs/salmonvideo",
 
     # Data settings
@@ -23,54 +19,64 @@ config = {
     # Data settings for LLFF
     "keyframes": False,
     "isg_step": 20000,
-    "ist_step": -1,
+    "ist_step": 40000,
 
     # Optimization settings
-    # "num_steps": 40001,
-    "num_steps": 40001,
+    # "num_steps": 90001,
+    "num_steps": 2001,
     "floater_loss": 0.0000,
     "regnerf_weight_start": 0,
     "regnerf_weight_end": 0.0,
     "regnerf_weight_max_step": 512,
-    "plane_tv_weight": 0.05,  
+    "plane_tv_weight": 0.0,  
+    "time_smoothness_weight": 1.0,
+    "l1_plane_color_reg": 0,
+    "l1_plane_density_reg": 0,
     "l1density_weight": 0,  # Not used for video yet
     "volume_tv_weight": 0.0,  # Not used for video yet
     "volume_tv_npts": 1024,  # Not used for video yet
     "volume_tv_what": "Gcoords",  # Not used for video yet
-    "scheduler_type": None, # "step"
+    "scheduler_type": "cosine", # "step"
     "batch_size": 4096,  
     "optim_type": "adam",
     "lr": 0.01,
     
     # Training settings
     "train_fp16": True,
-    "save_every": 10000,
-    "valid_every": 10000,
+    "save_every": 60000,
+    "valid_every": 60000,
     "save_video": True,
     "save_outputs": True,
 
     # Raymarching settings
     "raymarch_type": "fixed",
     "num_sample_multiplier": 2,
-    "n_intersections": 400,
-    "spacing_fn": "reciprocal",
+    "n_intersections": 128,
+    "spacing_fn": "linear",  # reciprocal. Seems to not be used if proposal sampling is used
     "single_jitter": False,
+    # Proposal sampling settings
+    "histogram_loss_weight": 1,  # this should be set > 0 when using proposal sampling
+    "density_field_resolution": [64, 64, 64],
+    "density_field_rank": 10,
+    "num_proposal_samples": 64,
 
     # Model settings
     "sh": True,
     "upsample_time_resolution": [150],
     # "upsample_time_steps": [6000],  # DyNerf does 300K iterations with keyframes, with lr 5e-4
     "upsample_time_steps": [-1],
-    "add_rank_steps": [100],
+    "use_trainable_rank": False,
+    "add_rank_steps": [-1],
     "use_F": False,
+    "multiscale_res": [1, 2, 4, 8],
     "grid_config": """
 [
     {
         "input_coordinate_dim": 4,
         "output_coordinate_dim": 28,
         "grid_dimensions": 2,
-        "resolution": [300, 300, 300, 150],
-        "rank": 2,
+        "resolution": [64, 64, 64, 150],
+        "rank": 1,
     },
     {
         "input_coordinate_dim": 1,
