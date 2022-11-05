ced2a3f9f0a10c594c2e22d0a348be37f87b7ad9
giacomo <giacomo.meanti@edu.unige.it>
2022-11-05 22:50:22 +0100

    Config

--- plenoxels/configs/giac_video.py
+++ plenoxels/configs/giac_video.py
@@ -1,7 +1,9 @@
 # configuration file to be used with `main.py` for video training
 config = {
-    "expname": "test_salmon_rank4",
-    "logdir": "./logs",
+    # "expname": "newmodel500.500.500.150_noF_rank2_reg30k_isg30k_ist30k_lr0.01cosine_l1sigma0.1_planetv0.05_sample400_init0.1to0.5",
+    # "expname": "multiscale64to512.150_rank4_reg20k_isg20k_ist20k_lr0.01_timesmooth1_planetv0_scale0.6xyshift2z",
+    "expname": "rank2",
+    "logdir": "./logs/salmonvideo",
 
     # Data settings
     "data_downsample": 4.0,
@@ -16,55 +18,66 @@ config = {
     "max_test_tsteps": 2,
     # Data settings for LLFF
     "keyframes": False,
-    "isg_step": 20000,
-    "ist_step": -20000,
+    "isg_step": 60000,
+    "ist_step": 120000,
 
     # Optimization settings
-    # "num_steps": 40001,
-    "num_steps": 40001,
+    "num_steps": 180001,
     "floater_loss": 0.0000,
     "regnerf_weight_start": 0,
     "regnerf_weight_end": 0.0,
     "regnerf_weight_max_step": 512,
-    "plane_tv_weight": 0.02,
-    "l1density_weight": 0,  # Not used for video yet
-    "volume_tv_weight": 0.0,  # Not used for video yet
-    "volume_tv_npts": 1024,  # Not used for video yet
+    "plane_tv_weight": 0.001,
+    "time_smoothness_weight": 1.0,
+    "l1_plane_color_reg": 0,
+    "l1_plane_density_reg": 0,
+    "l1density_weight": 0,     # Not used for video yet
+    "volume_tv_weight": 0.0,   # Not used for video yet
+    "volume_tv_npts": 1024,    # Not used for video yet
     "volume_tv_what": "Gcoords",  # Not used for video yet
-    "scheduler_type": "warmup_cosine", # "step"
+    "scheduler_type": "warmup_step_many", # "step"
     "batch_size": 4096,
     "optim_type": "adam",
     "lr": 0.01,
 
     # Training settings
     "train_fp16": True,
-    "save_every": 10000,
-    "valid_every": 10000,
+    "save_every": 23999,
+    "valid_every": 29999,
     "save_video": True,
     "save_outputs": True,
 
     # Raymarching settings
     "raymarch_type": "fixed",
     "num_sample_multiplier": 2,
-    "n_intersections": 400,
-    "spacing_fn": "reciprocal",
+    "n_intersections": 48,
+    "spacing_fn": "linear",  # reciprocal. Seems to not be used if proposal sampling is used
     "single_jitter": False,
+    # Proposal sampling settings
+    "histogram_loss_weight": 1.0,  # this should be set > 0 when using proposal sampling
+    "density_field_resolution": [128, 256],
+    "density_field_rank": 10,
+    "num_proposal_samples": [256, 96],
 
     # Model settings
     "sh": True,
+    "sh_decoder_type": "manual",
+    "density_activation": "trunc_exp",
     "upsample_time_resolution": [150],
     # "upsample_time_steps": [6000],  # DyNerf does 300K iterations with keyframes, with lr 5e-4
     "upsample_time_steps": [-1],
+    "use_trainable_rank": False,
+    "add_rank_steps": [-1],
     "use_F": False,
-    "add_rank_steps": [100, 101, 102],
+    "multiscale_res": [1, 2, 4],
     "grid_config": """
 [
     {
         "input_coordinate_dim": 4,
         "output_coordinate_dim": 28,
         "grid_dimensions": 2,
-        "resolution": [300, 300, 300, 150],
-        "rank": 4,
+        "resolution": [80, 64, 64, 150],
+        "rank": 2,
     },
     {
         "input_coordinate_dim": 1,
