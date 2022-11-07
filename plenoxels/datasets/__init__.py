03b530d10197c198f717a760d2a67c383db5630f
gmeanti <giacomo.meanti@gmail.com>
2022-11-07 12:37:44 +0100

    [WIP] base dataset to accept 'weights_subsampled' param for subsampling batch

--- plenoxels/datasets/__init__.py
+++ plenoxels/datasets/__init__.py
@@ -1,12 +1,11 @@
 from .llff_dataset import LLFFDataset
 from .synthetic_nerf_dataset import SyntheticNerfDataset
-from .video_datasets import VideoLLFFDataset, Video360Dataset
+from .video_datasets import Video360Dataset
 from .photo_tourism import PhotoTourismDataset
 
 __all__ = (
     "LLFFDataset",
     "SyntheticNerfDataset",
-    "VideoLLFFDataset",
     "Video360Dataset",
     "PhotoTourismDataset",
 )
