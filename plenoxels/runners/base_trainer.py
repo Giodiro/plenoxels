81725cb35cfe92aa76ab7b759d2f7d7db741a40f
giacomo m <giacomo.meanti@gmail.com>
2022-12-03 11:14:51 +0100

    Fixes for reloading

--- plenoxels/runners/base_trainer.py
+++ plenoxels/runners/base_trainer.py
@@ -270,6 +270,7 @@ class BaseTrainer():
             logging.info("=> Loaded scheduler state from checkpoint")
 
         self.global_step = checkpoint_data["global_step"]
+        self.nerfacc_helper.step_cb(self.global_step)
         logging.info(f"=> Loaded step {self.global_step} from checkpoints")
 
     @abc.abstractmethod
@@ -315,6 +316,8 @@ class BaseTrainer():
     def init_optim(self, **kwargs) -> torch.optim.Optimizer:
         if self.optim_type == 'adam':
             optim = torch.optim.Adam(params=self.model.get_params(kwargs['lr']), eps=1e-15)
+        elif self.optim_type == 'rmsprop':
+            optim = torch.optim.RMSprop(params=self.model.get_params(kwargs['lr']))
         else:
             raise NotImplementedError()
         return optim
@@ -370,15 +373,11 @@ class NerfaccHelper():
 
     @property
     def density_threshold(self):
-        #if self.global_step < 512:
-        #    return self._density_threshold# / 10
-        return self._density_threshold
+        return min(self._density_threshold, self._density_threshold * self.global_step / 512)
 
     @property
     def alpha_threshold(self):
-        if self.global_step < 512:
-            return self._alpha_threshold
-        return self._alpha_threshold
+        return min(self._alpha_threshold, self._alpha_threshold * self.global_step / 512)
 
     @property
     def max_rays(self):
